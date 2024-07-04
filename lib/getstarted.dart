f3f51dcde890617e4f1786f202d65fa40bde8852
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-04 11:42:01 +0530

    data added

--- lib/getstarted.dart
+++ lib/getstarted.dart
@@ -21,6 +21,52 @@ class _GetStartedPageState extends State<GetStartedPage> {
               width: 145,
             ),
           ),
+          SizedBox(
+            height: 55,
+          ),
+          Image.asset(
+            'assets/Group 58.png',
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Text(
+            'Streamline your \n  Attendance process \n with Attendix',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3370B6),
+              fontSize: 25,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              height: 0,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 288,
+            height: 43,
+            decoration: ShapeDecoration(
+              color: Color(0xFF2C86C8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'Get Started',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
