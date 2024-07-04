c599aa796e2505755a44adbb26f307a1aed27a76
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-04 11:57:04 +0530

    allignment  fixed

--- lib/getstarted.dart
+++ lib/getstarted.dart
@@ -17,52 +17,53 @@ class _GetStartedPageState extends State<GetStartedPage> {
           Center(
             child: Image.asset(
               'assets/logo.png',
-              height: 25,
-              width: 145,
+              height: 30,
+              width: 150,
             ),
           ),
           SizedBox(
-            height: 55,
+            height: 40,
           ),
           Image.asset(
             'assets/Group 58.png',
+            width: double.infinity,
           ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
+          SizedBox(height: 30),
           Text(
             'Streamline your \n  Attendance process \n with Attendix',
             textAlign: TextAlign.center,
             style: TextStyle(
               color: Color(0xFF3370B6),
-              fontSize: 25,
+              fontSize: 27,
               fontFamily: 'Poppins',
               fontWeight: FontWeight.w600,
               height: 0,
             ),
           ),
           SizedBox(
-            height: 10,
+            height: 30,
           ),
-          Container(
-            width: 288,
-            height: 43,
-            decoration: ShapeDecoration(
-              color: Color(0xFF2C86C8),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Container(
+              width: double.infinity,
+              height: 43,
+              decoration: ShapeDecoration(
+                color: Color(0xFF2C86C8),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
               ),
-            ),
-            child: Center(
-              child: Text(
-                'Get Started',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600,
-                  height: 0,
+              child: Center(
+                child: Text(
+                  'Get Started',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
