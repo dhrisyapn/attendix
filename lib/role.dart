fd576ec64427ec07febfc00fa2ba92377dd6cc74
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-11 20:09:21 +0530

    select role page created

---
+++ lib/role.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectRolePage extends StatefulWidget {
+  const SelectRolePage({super.key});
+
+  @override
+  State<SelectRolePage> createState() => _SelectRolePageState();
+}
+
+class _SelectRolePageState extends State<SelectRolePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
