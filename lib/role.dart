d508bc396544879055cd20e1fcfdd6c8abb555a9
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-13 13:06:02 +0530

    button added

--- lib/role.dart
+++ lib/role.dart
@@ -1,4 +1,6 @@
 import 'package:attendix/login.dart';
+import 'package:attendix/studentlogin.dart';
+import 'package:attendix/teacherlogin.dart';
 import 'package:flutter/material.dart';
 
 class SelectRolePage extends StatefulWidget {
@@ -72,23 +74,31 @@ class _SelectRolePageState extends State<SelectRolePage> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              width: 282,
-              height: 31,
-              decoration: ShapeDecoration(
-                color: Color(0xFF2C86C8),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-              ),
-              child: Center(
-                child: Text(
-                  'Teacher',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TeacherLoginPage()));
+              },
+              child: Container(
+                width: 282,
+                height: 31,
+                decoration: ShapeDecoration(
+                  color: Color(0xFF2C86C8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+                child: Center(
+                  child: Text(
+                    'Teacher',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
@@ -96,23 +106,31 @@ class _SelectRolePageState extends State<SelectRolePage> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              width: 282,
-              height: 31,
-              decoration: ShapeDecoration(
-                color: Color(0xFF2C86C8),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-              ),
-              child: Center(
-                child: Text(
-                  'Student',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StudentLoginPage()));
+              },
+              child: Container(
+                width: 282,
+                height: 31,
+                decoration: ShapeDecoration(
+                  color: Color(0xFF2C86C8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+                child: Center(
+                  child: Text(
+                    'Student',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
