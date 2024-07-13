c15771d05bb6f5a0dbffb474aea1616dea1d4f20
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-13 18:45:44 +0530

    Create teacherhome.dart

---
+++ lib/teacherhome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TeacherHomePage extends StatefulWidget {
+  const TeacherHomePage({super.key});
+
+  @override
+  State<TeacherHomePage> createState() => _TeacherHomePageState();
+}
+
+class _TeacherHomePageState extends State<TeacherHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
