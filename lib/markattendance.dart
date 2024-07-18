72a589ec78eb26ce4cefd0636959d06f386440a5
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-18 19:13:47 +0530

    Create markattendance.dart

---
+++ lib/markattendance.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MarkAttendancePage extends StatefulWidget {
+  const MarkAttendancePage({super.key});
+
+  @override
+  State<MarkAttendancePage> createState() => _MarkAttendancePageState();
+}
+
+class _MarkAttendancePageState extends State<MarkAttendancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
