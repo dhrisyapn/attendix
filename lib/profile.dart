37a71adf1a2b73ef726c15432cee0c30ea93ed8f
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-07-19 18:35:29 +0530

    Create profile.dart

---
+++ lib/profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SudentProfilePage extends StatefulWidget {
+  const SudentProfilePage({super.key});
+
+  @override
+  State<SudentProfilePage> createState() => _SudentProfilePageState();
+}
+
+class _SudentProfilePageState extends State<SudentProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
